697c64dc38762e75499ac53ea04f47f91f37a52a
Deep Maheshwari <deepmaheshwari123dm@gmail.com>
2025-02-18 20:55:08 +0530

    Changes

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,12 +1,14 @@
 import os
 
 AWS_S3_BUCKET_NAME = ""
-MONGO_DATABASE_NAME = ""
-MONGO_COLLECTION_NAME ="" 
+MONGO_DATABASE_NAME = "SensorFault"
+MONGO_COLLECTION_NAME ="waferfault" 
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL = ""
+MONGO_DB_URL = "mongodb+srv://sensor:12345@data0.eu62c.mongodb.net/?retryWrites=true&w=majority&appName=Data0"
 
 MODEL_FILE_NAME = "model" 
 MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder = "artifacts"
